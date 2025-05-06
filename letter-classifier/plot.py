330288c4f8962bec75d9c22e5f1af187c31f4d91
Mojtaba <mojtabamomenaei@gmail.com>
2025-05-06 10:11:08 +0330

    init

---
+++ letter-classifier/plot.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# Creating data set
+
+# A
+a =[0, 0, 1, 1, 0, 0,
+   0, 1, 0, 0, 1, 0,
+   1, 1, 1, 1, 1, 1,
+   1, 0, 0, 0, 0, 1,
+   1, 0, 0, 0, 0, 1]
+# B
+b =[0, 1, 1, 1, 1, 0,
+   0, 1, 0, 0, 1, 0,
+   0, 1, 1, 1, 1, 0,
+   0, 1, 0, 0, 1, 0,
+   0, 1, 1, 1, 1, 0]
+# C
+c =[0, 1, 1, 1, 1, 0,
+   0, 1, 0, 0, 0, 0,
+   0, 1, 0, 0, 0, 0,
+   0, 1, 0, 0, 0, 0,
+   0, 1, 1, 1, 1, 0]
+
+# Creating labels
+y =[[1, 0, 0],
+   [0, 1, 0],
+   [0, 0, 1]]
+
+
+
+
+# visualizing the data, plotting A.
+plt.imshow(np.array(c).reshape(5, 6))
+plt.show()
